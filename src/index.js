1d6b7cfbcbf45a4f55ad618f8c8fcfedc8c03522
CFA19 <cfa0819@gmail.com@gmail.com>
2023-11-28 13:58:30 -0300

    First commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import root  from 'react-dom';
+
+import { GifExpertApp } from './GifExpertApp';
+
+import './index.css';
+
+root.render(
+  <GifExpertApp />,
+  document.getElementById('root')
+);
+
